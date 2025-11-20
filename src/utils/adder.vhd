f79b9632a7299b6484b4ee8fc26b86fa13dab213
Eduqtr <eddy.dago@uqtr.ca>
2025-11-20 00:51:35 -0500

    Create adder.vhd

---
+++ src/utils/adder.vhd
@@ -0,0 +1,14 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;  
+
+
+entity adder is
+    port(a, b: in STD_LOGIC_VECTOR(31 downto 0);
+         y: out STD_LOGIC_VECTOR(31 downto 0));
+end;
+
+architecture behave of adder is
+begin
+    y <= std_logic_vector(unsigned(a) + unsigned(b));
+end;
